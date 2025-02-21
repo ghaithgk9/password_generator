f7fe1ea0c7e2be03b267426af47b67c2aeaca2cb
Ghaith kouki <ghaithkouki39@gmail.com>
2025-02-21 17:09:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const lengthInput = document.querySelector(".leng");
+const upper = document.getElementById("upper");
+const lower = document.getElementById("lower");
+const symb = document.getElementById("sym");
+const result = document.getElementById("result");
+const generateBtn = document.querySelector("button");
+
+function generatePassword() {
+    const low_chars = "abcdefghijklmnopqrstuvwxyz";
+    const upp_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const sym_chars = "!&_ç@$£%*";
+    const num_chars = "0123456789";
+
+    let characters = "";
+    let password = "";
+    let length = parseInt(lengthInput.value) || 8;
+
+    if (lower.checked) characters += low_chars;
+    if (upper.checked) characters += upp_chars;
+    if (symb.checked) characters += sym_chars;
+    characters += num_chars;
+
+    if (characters.length === 0) {
+        result.textContent = "Select at least one option!";
+        return;
+    }
+
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    result.textContent = `Your password is: ${password}`;
+}
+
+generateBtn.addEventListener("click", generatePassword);
+
+
